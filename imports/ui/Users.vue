dcd250d1a37275ebafa4ce2e4d266d23172109a6
Gusto <gumisgusto@email.com>
2016-12-30 15:18:50 +0100

    Typo

--- imports/ui/Users.vue
+++ imports/ui/Users.vue
@@ -2,7 +2,7 @@
 <div>
 	<h2>List of users</h2>
   <div v-if="users.length">
-    <p v-for="user in users">1 {{user.username}} ({{user.emails[0].address}})</p>
+    <p v-for="user in users">{{user.username}} ({{user.emails[0].address}})</p>
   </div>
 	<p v-else>There are no users yet. Why don't you register some?</p>
 </div>
