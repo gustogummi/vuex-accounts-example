e5c45ec70c06f220468d91d2d99bbcb977ad90ea
gustogummi <gumisgusto@email.com>
2017-01-18 23:03:14 +0100

    Update Register.vue

--- imports/ui/auth/Register.vue
+++ imports/ui/auth/Register.vue
@@ -1,21 +1,20 @@
 <template>
 <div>
-	<form @submit.prevent="submitForm">
-		<h3>Register</h3>
-		<label>username</label>
-		<input v-model="formData.username"/>
+  <form @submit.prevent="submitForm">
+    <h3>Register</h3>
+    <label>username</label>
+    <input v-model="formData.username"/>
     <label>email</label>
     <input v-model="formData.email"/>
-		<label>password</label>
-		<input v-model="formData.password" />
-		<button>Register</button>
+    <label>password</label>
+    <input v-model="formData.password" />
+    <button>Register</button>
   </form>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'register',
   data() {
     return {
       formData: {
@@ -27,7 +26,7 @@ export default {
   },
   methods: {
     submitForm() {
-  		this.$store.dispatch('submitRegisterForm', this.formData)
+      this.$store.dispatch('submitRegisterForm', this.formData)
     }
   }
 }
